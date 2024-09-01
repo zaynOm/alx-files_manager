2bb4bcde9eea26b45ba263c591345237e59b486a
zaynom <ouaziz0626@gmail.com>
2024-09-01 18:43:22 +0100

    Task 1: MongoDB utils

--- utils/db.js
+++ utils/db.js
@@ -1,1 +1,32 @@
-#!/usr/bin/node
+import { MongoClient } from 'mongodb';
+
+const dbHost = process.env.DB_HOST || 'localhost';
+const dbPort = process.env.DB_port || 27017;
+const dbName = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${dbHost}:${dbPort}`;
+
+class DBClient {
+  constructor() {
+    this.client = new MongoClient(url, {
+      useUnifiedTopology: true,
+    });
+    this.client.connect().then(() => {
+      this.db = this.client.db(dbName);
+    });
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    return this.db.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.db.collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
