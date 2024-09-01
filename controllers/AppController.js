f5f33739307f7a6f4cebd8b82f3716e1a79e1ed6
zaynom <ouaziz0626@gmail.com>
2024-09-01 19:21:30 +0100

    Task 2: First API
    define 2 endpoints in the AppController

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,1 +1,17 @@
-#!/usr/bin/node
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+class AppController {
+  static getStatus(req, res) {
+    const status = { redis: redisClient.isAlive(), db: dbClient.isAlive() };
+    res.status(200).json(status);
+  }
+
+  static async getStats(req, res) {
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+    res.status(200).json({ users, files });
+  }
+}
+
+export default AppController;
