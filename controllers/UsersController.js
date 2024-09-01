68071018f1282d1d4416d44a52272c97c0bfc176
zaynom <ouaziz0626@gmail.com>
2024-09-01 22:59:20 +0100

    Task 3: Create a new user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,1 +1,26 @@
-#!/usr/bin/node
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
+
+async function postNew(req, res) {
+  const { email, password } = req.body;
+  if (!email) {
+    return res.status(400).send({ error: 'Missing email' });
+  }
+  if (!password) {
+    return res.status(400).send({ error: 'Missing password' });
+  }
+
+  const users = await dbClient.db.collection('users');
+  const user = await users.findOne({ email });
+  if (user) {
+    return res.status(400).json({ error: 'Already exist' });
+  }
+
+  const hashedPassword = sha1(password);
+  const newUser = await users.insertOne({ email, password: hashedPassword });
+  return res.status(201).json({ id: newUser.id, email });
+}
+
+const UsersController = { postNew };
+
+export default UsersController;
