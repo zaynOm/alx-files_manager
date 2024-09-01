40ea0335948fde84f050dd1191c5a26ba9d4da70
zaynom <ouaziz0626@gmail.com>
2024-09-01 19:22:25 +0100

    Task 2: First API
    api endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+import express from 'express';
+import AppController from '../controllers/AppController';
+
+const router = express.Router();
+
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+
+export default router;
