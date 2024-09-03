5fe046b1eda641794130b9e4d89f6d619ba62f37
zaynom <ouaziz0626@gmail.com>
2024-09-03 23:42:49 +0100

    rename the utils file add getUserByToken func

---
+++ utils/utils.js
@@ -0,0 +1,29 @@
+import { ObjectId } from 'mongodb';
+import dbClient from './db';
+import redisClient from './redis';
+
+export function basicAuthDecoder(auth) {
+  const credentials = auth.split(' ')[1];
+  const decodedCredentials = Buffer.from(credentials, 'base64').toString(
+    'utf8',
+  );
+  return decodedCredentials.split(':');
+}
+
+export async function getUserByToken(res, token) {
+  if (!token) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  const userId = await redisClient.get(`auth_${token}`);
+  if (!userId) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  const user = await dbClient.findOne('users', { _id: ObjectId(userId) });
+  if (!user) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  return user;
+}
