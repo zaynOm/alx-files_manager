5a03bda43a0f4250aabe4717a268eecc4050158c
zaynom <ouaziz0626@gmail.com>
2024-09-03 23:41:57 +0100

    rename the utils file to make it more general

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,7 +1,7 @@
 import sha1 from 'sha1';
 import { v4 as uuidv4 } from 'uuid';
 import { ObjectId } from 'mongodb';
-import basicAuthDecoder from '../utils/authUtils';
+import basicAuthDecoder from '../utils/utils';
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
