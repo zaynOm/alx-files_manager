e2399242eee560491529ca316155a650cd3cef6c
zaynom <ouaziz0626@gmail.com>
2024-09-03 18:35:03 +0100

    Task 5: First file
    file upload controller

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,1 +1,76 @@
-#!/usr/bin/node
+import { ObjectId } from 'mongodb';
+import { v4 as uuidv4 } from 'uuid';
+import { promises as fs } from 'fs';
+import path from 'path';
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+class FilesController {
+  static async postUpload(req, res) {
+    const token = req.header('x-token');
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const user = dbClient.findOne('users', { _id: ObjectId(userId) });
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const {
+      name, type, parentId = 0, isPublic = false, data,
+    } = req.body;
+    if (!name) {
+      return res.status(400).json({ error: 'Missing name' });
+    }
+    if (!type || !['folder', 'file', 'image'].includes(type)) {
+      return res.status(400).json({ error: 'Missing type' });
+    }
+    if (!data && type !== 'folder') {
+      return res.status(400).json({ error: 'Missing data' });
+    }
+
+    if (parentId) {
+      const file = await dbClient.findOne('files', { _id: ObjectId(parentId) });
+      if (!file) {
+        return res.status(400).json({ error: 'Parent not found' });
+      }
+      if (file.type !== 'folder') {
+        return res.status(400).json({ error: 'Parent is not a folder' });
+      }
+    }
+
+    const newFile = {
+      userId,
+      name,
+      type,
+      isPublic,
+      parentId,
+    };
+
+    if (type !== 'folder') {
+      const localFileName = uuidv4();
+      const folderPath = process.env.FOLDER_PATH || '/tmp/files_manager';
+      const localPath = path.join(folderPath, localFileName);
+
+      const decodedData = Buffer.from(data, 'base64');
+
+      await fs.mkdir(folderPath, { recursive: true });
+      await fs.writeFile(localPath, decodedData);
+
+      newFile.localPath = localPath;
+    }
+
+    // spread the new file so the _id doesn't get added to it
+    const fileResult = await dbClient.insertOne('files', { ...newFile });
+    delete newFile.localPath;
+    return res.status(201).json({ id: fileResult.insertedId, ...newFile });
+  }
+}
+
+export default FilesController;
