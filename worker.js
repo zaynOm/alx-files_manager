03c925bae43b555fae40d7ac98e82244ac6cf6cb
zaynom <ouaziz0626@gmail.com>
2024-09-04 23:49:14 +0100

    Task 9:  Image Thumbnails
    fix files worker

--- worker.js
+++ worker.js
@@ -1,35 +1,36 @@
 import Queue from 'bull';
 import fs from 'fs';
 import imageThumbnail from 'image-thumbnail';
+import { ObjectId } from 'mongodb';
 import dbClient from './utils/db';
 
 export const fileQueue = new Queue('files');
 
 fileQueue.process(async (job) => {
-  const { fileId, userId } = job.data;
-
-  if (!fileId) {
-    throw new Error('Missing fileId');
-  }
-  if (!userId) {
-    throw new Error('Missing userId');
-  }
-
-  const file = await dbClient.findOne('files', { _id: fileId, userId });
-  if (!file) {
-    throw new Error('File not found');
-  }
+  console.log('Processing thumbnails...');
 
   try {
+    const { fileId, userId } = job.data;
+    if (!fileId) {
+      throw new Error('Missing fileId');
+    }
+    if (!userId) {
+      throw new Error('Missing userId');
+    }
+    const file = await dbClient.findOne('files', { _id: ObjectId(fileId), userId: ObjectId(userId) });
+    if (!file) {
+      throw new Error('File not found');
+    }
     const widths = [100, 250, 500];
     const thumbnailPromises = widths.map(async (width) => {
-      const thumbnail = await imageThumbnail(file.localePath, { width });
-      const thumbnailPath = `${file.localePath}_${width}`;
+      const thumbnail = await imageThumbnail(file.localPath, { width });
+      const thumbnailPath = `${file.localPath}_${width}`;
       await fs.promises.writeFile(thumbnailPath, thumbnail);
     });
 
     return await Promise.all(thumbnailPromises);
   } catch (error) {
-    return null;
+    console.error(`Error: ${error.message}`);
   }
+  return null;
 });
