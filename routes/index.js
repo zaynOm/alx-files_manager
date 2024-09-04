bb18e815db2c3166b90735ac96ccb13898cb6a05
zaynom <ouaziz0626@gmail.com>
2024-09-04 12:04:51 +0100

    Task 7: File publish/unpublish
    add routes

--- routes/index.js
+++ routes/index.js
@@ -20,4 +20,7 @@ router.post('/files', FilesController.postUpload);
 router.get('/files/:id', FilesController.getShow);
 router.get('/files', FilesController.getIndex);
 
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+
 export default router;
