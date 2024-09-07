39525f54949a7969fe621f989772e3d9fa0935b3
zaynom <ouaziz0626@gmail.com>
2024-09-07 01:21:24 +0100

    fix eslint issue
    remove unused import

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,6 +1,5 @@
 import sha1 from 'sha1';
 import { v4 as uuidv4 } from 'uuid';
-import { ObjectId } from 'mongodb';
 import { basicAuthDecoder, getUserByToken } from '../utils/utils';
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
